507dc421c2a314c876cf07c00c85f668318cd33e
vicky <vickyy8689@gmail.com>
2024-04-11 12:55:25 +0530

    String updated

---
+++ String_And_Userinput/String_And_Userinput/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_And_Userinput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------------user input ----------------------------------------------------------------
+
+            //String str = Console.ReadLine();
+            //Console.WriteLine("hello "+str);
+            //Console.Write("How many canday you want "+str+ " " );
+            //String can  = Console.ReadLine();
+            //Console.WriteLine("you got 4 extra candy " +( Convert.ToInt32(can) + 4));
+
+            //----------------Math class--------------------------------------------------------------------------
+            //int a = Math.Max(45, 67);
+            //Console.WriteLine(a);
+            //int b = Math.Min(45, 5);
+            //Console.WriteLine(b);
+            //Console.WriteLine(Math.Sqrt(4));
+
+            //double x = Math.Sqrt(4);
+            //Console.WriteLine(x);
+
+
+            //------------------string-----------------------------------------------------------------------------
+
+            //String hello = "hello my name is vicky";
+            //Console.WriteLine(hello.Length);
+            //Console.WriteLine(hello);
+            //Console.WriteLine(hello.ToUpper());
+            //Console.WriteLine(hello.ToLower());
+            //Console.WriteLine(hello + " you are ok ");
+            //Console.WriteLine(string.Concat(hello , " you are ok "));
+
+            //--------string inter poletion -----------------------------------------
+            //string name  = Console.ReadLine();
+            //string can = Console.ReadLine();
+            //Console.WriteLine($"your name is {name}. you got {can} candies. ");
+
+
+            //access charater in string ------------
+            //String hello = "hello my name\" is vicky";
+            //Console.WriteLine(hello[0]);
+            //Console.WriteLine(hello.IndexOf("is"));
+            //Console.WriteLine(hello.Substring(1));   //sub string 
+
+        }
+    }
+}
