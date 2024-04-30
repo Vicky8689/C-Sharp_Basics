2e4d61dd6304e717a7ae65519c39139b1d4e7adf
vicky <vickyy8689@gmail.com>
2024-04-30 15:39:38 +0530

    OOPS [vicky]: work on oops concepts

--- OOPS/OOPS/Program.cs
+++ OOPS/OOPS/Program.cs
@@ -2,13 +2,47 @@
 
 namespace Oop
 {
-  in class program
+   class program
     {
-        
+        public class A
+        {
+            public int code ;
+            public string name ;
+            public virtual void print()
+            {
+                Console.WriteLine("i am in A printing "+code+" "+name);
+            }
+        }
+        public class B :A
+        {
+            
+            public string address = "mankhurd";
+            public override void print()
+            {
+                Console.WriteLine("i am in B printing " + address);
+            }
+
+        }
+
 
         static void Main(string[] argus)
         {
-            Console.WriteLine("hii");
+
+            A a = new A();
+            a.code = 45;
+            a.name = "vicky";
+            a.print();
+
+            B b = new B();
+            b.print();
+
+            A c = new B();
+            
+            
+            c.print();
+
+
+           
             
         }
     }
